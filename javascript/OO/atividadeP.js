6b1912c6fc022f790be350fdbb14c1392a3cf565
Rhyan Gabriel <rhyan.belem@aluno.senai.br>
2026-08-13 14:16:24 -0300

    2026/08/13

---
+++ javascript/OO/atividadeP.js
@@ -0,0 +1,46 @@
+class Produto {
+    constructor(nome, preço, categoria, desconto) {
+        this.nome = nome;
+        this.preço = preço;
+        this.categoria = categoria;
+        this.desconto = desconto;
+    }
+
+    aplicarDesconto() {
+        return this.preço - (this.preço * (this.desconto / 100));
+    }
+
+    exibirInformacoes() {
+    const resultado = document.querySelector('#resultado');
+    const precoFinal = this.aplicarDesconto();
+
+    resultado.innerHTML = `
+        <div>
+            <p>Nome: ${this.nome}</p>
+            <p>Preço: R$${precoFinal.toFixed(2)}</p>
+            <p>Categoria: ${this.categoria}</p>
+            <p>Desconto: ${this.desconto}%</p>
+        </div>
+    `;
+    }
+}
+
+const produto = new Produto () ;
+const nome = document.querySelector('#nome');
+const preço = document.querySelector('#preço');
+const categoria = document.querySelector('#categoria');
+const desconto = document.querySelector('#desconto');
+const botaoCadastrar = document.querySelector('#botaoCadastrar');
+
+botaoCadastrar.addEventListener('click', function () {
+
+    const novoProduto = new Produto(
+        nome.value,
+        parseFloat(preço.value),
+        categoria.value,
+        parseFloat(desconto.value)
+    );
+
+    novoProduto.exibirInformacoes();
+    novoProduto.aplicarDesconto();
+});
---
+++ javascript/OO/atividadeP2.js
@@ -0,0 +1,68 @@
+class Produto {
+
+    constructor(nome, preco, categoria, desconto) {
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+        this.desconto = desconto;
+    }
+
+    aplicarDesconto() {
+        return this.preco - (this.preco * (this.desconto / 100));
+    }
+
+}
+
+class Estoque {
+
+    constructor() {
+        this.produtos = [];
+    }
+
+    adicionarProduto(produto) {
+        this.produtos.push(produto);
+    }
+
+    exibirNaTela() {
+
+        const resultado = document.querySelector('#resultado');
+        resultado.innerHTML = "";
+
+        this.produtos.forEach(produto => {
+
+            resultado.innerHTML += `
+                <div>
+                    <p>Nome: ${produto.nome}</p>
+                    <p>Preço: R$${produto.aplicarDesconto().toFixed(2)}</p>
+                    <p>Categoria: ${produto.categoria}</p>
+                    <p>Desconto: ${produto.desconto}%</p>
+                </div>
+            `;
+
+        });
+
+    }
+
+}
+
+const estoque = new Estoque();
+
+const nome = document.querySelector('#nome');
+const preco = document.querySelector('#preco');
+const categoria = document.querySelector('#categoria');
+const desconto = document.querySelector('#desconto');
+const botaoCadastrar = document.querySelector('#botaoCadastrar');
+
+botaoCadastrar.addEventListener('click', function () {
+
+    const novoProduto = new Produto(
+        nome.value,
+        parseFloat(preco.value),
+        categoria.value,
+        parseFloat(desconto.value)
+    );
+
+    estoque.adicionarProduto(novoProduto);
+    estoque.exibirNaTela();
+
+});
